460f41c5bb76e594dac925b3152bd6c787d534d1
sunfishes <cowjung1024@naver.com>
2025-05-11 00:48:52 +0900

    Initial Commit

---
+++ lib/splash_screen.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class SplashScreen extends StatelessWidget {
+  const SplashScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Color(0xFF16212C),
+      body: Center(child: Image.asset('assets/business_card.png',),),
+    );
+  }
+}
